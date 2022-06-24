c0aab048ee992db1491f43d5f6f9674702f50d71
Nancy Cai <nancycai@live.com>
2022-06-24 14:28:05 +1000

    Create output.tf for example

---
+++ example/output.tf
@@ -0,0 +1,11 @@
+output "bucket-arn" {
+  value = module.example-bucket.bucket-arn
+}
+
+output "replication-bucket-arn" {
+  value = module.example-bucket.replication-bucket-arn
+}
+
+output "bucket-name" {
+  value = module.example-bucket.bucket-name
+}
